6a9250f6ad09951f653edc4b15a8c53831a17d02
melkamu <zmelkamu918@gmail.com>
2023-05-02 15:45:49 +0300

    intial structured get setup

---
+++ app/src/main/java/com/example/musicplayerwithjava/ManuActivity.java
@@ -0,0 +1,14 @@
+package com.example.musicplayerwithjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manu);
+    }
+}
